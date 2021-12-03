c09d54d0bec31497fcaf081f87fa07c65ce1df6d
EG-easy <1018eguchi@gmail.com>
2021-12-03 12:46:23 +0900

    refactor deploy cmd

---
+++ cmd/deploy/deploy-janken.js
@@ -0,0 +1,19 @@
+// @ts-check
+const initClient = require('../../client/client')
+const { Executor } = require('../../executor/executor')
+
+const main = async () => {
+  const client = await initClient()
+  const contractAddress = process.env.SECRET_NFT_CONTRACT
+  const executor = new Executor(client, contractAddress)
+
+  const wasmPATH = '../../janken.wasm'
+  const initMsg = {}
+  const contractName = `My Janken${Math.ceil(Math.random() * 10000)}`
+
+  await executor.deploy(wasmPATH, initMsg, contractName)
+}
+
+main().catch((err) => {
+  console.error(err)
+})
