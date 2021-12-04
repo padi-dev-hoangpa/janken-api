cd66f2eae5e01ce22f43bb4a3a8d34d483f957f2
EG-easy <1018eguchi@gmail.com>
2021-12-04 19:27:18 +0900

    add fetchAllNFTs test
    fix test

---
+++ tests/__tests__/services/fetchAllNFTs.js
@@ -0,0 +1,16 @@
+const ApiService = require('../../../service/ApiService')
+const { OrmWrapper } = require('../../../service/orm/OrmWrapper')
+
+const orm = new OrmWrapper()
+const service = new ApiService(null, orm)
+
+describe('API Service', () => {
+  test('fetchAllNFTs()', async () => {
+    const NFTs = await service.fetchAllNFTs()
+
+    expect(NFTs.length).toBe(3)
+    expect(NFTs[0].tokenId).toBe('0001')
+    expect(NFTs[0].owner).toBe('secret19398wwp6yksvthgczcw9upphsx4qnx9hnu69l3')
+    expect(NFTs[0].image).toBe('https://example.com/1')
+  })
+})
