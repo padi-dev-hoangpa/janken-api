9dd385ee5a0bf7d89453ebfa102819655352c876
EG-easy <1018eguchi@gmail.com>
2021-12-08 12:00:26 +0900

    add winner

--- service/ApiService.js
+++ service/ApiService.js
@@ -145,6 +145,7 @@ class ApiService {
     const response = await this.jankenExecutor.queryOffer(offer.offerId)
     offer.offereeHands = JSON.stringify(args.input.offereeHands)
     offer.status = response.status
+    offer.winner = response.winner === 'offeror' ? response.offeror : response.offeree
 
     await this.orm.updateOffer(offer)
     return { status: 'ok' }
